51e1e5994792d0bcaabda8b7b1d6283140d8ef38
seriaati <seria.ati@gmail.com>
2023-10-22 22:42:13 +0800

    refactor: update models

--- hoyo_buddy/db/models.py
+++ hoyo_buddy/db/models.py
@@ -1,23 +1,34 @@
 from enum import StrEnum
 from typing import Any, Dict, Optional
 
+import genshin
+from discord import Locale
 from tortoise import fields
 from tortoise.models import Model
 
 
 class Game(StrEnum):
-    GENSHIN = "genshin"  # Genshin Impact
-    STARRAIL = "hkrpg"  # Honkai: Star Rail
-    HONKAI = "honkai3rd"  # Honkai Impact 3rd
+    GENSHIN = "Genshin Impact"
+    STARRAIL = "Honkai: Star Rail"
+    HONKAI = "Honkai Impact 3rd"
+
+
+GAME_CONVERTER = {
+    Game.GENSHIN: genshin.Game.GENSHIN,
+    Game.STARRAIL: genshin.Game.STARRAIL,
+    Game.HONKAI: genshin.Game.HONKAI,
+}
 
 
 class User(Model):
-    id = fields.BigIntField(pk=True, index=True)
-    accounts = fields.ManyToManyField("models.HoyoAccount", related_name="users")
+    id = fields.BigIntField(pk=True, index=True, generated=False)
+    accounts: fields.ManyToManyRelation["HoyoAccount"] = fields.ManyToManyField(
+        "models.HoyoAccount", related_name="users"
+    )
     settings: fields.OneToOneRelation["Settings"] = fields.OneToOneField(
         "models.Settings", related_name="user"
     )
-    temp_data: Dict[str, Any] = fields.JSONField()  # type: ignore
+    temp_data: Dict[str, Any] = fields.JSONField(default=dict)  # type: ignore
 
 
 class HoyoAccount(Model):
@@ -31,7 +42,17 @@ class HoyoAccount(Model):
     class Meta:
         unique_together = ("uid", "game")
 
+    @property
+    def client(self) -> genshin.Client:
+        return genshin.Client(
+            self.cookies, game=GAME_CONVERTER[self.game], uid=self.uid
+        )
+
 
 class Settings(Model):
     lang: Optional[str] = fields.CharField(max_length=5, null=True)  # type: ignore
     user: fields.BackwardOneToOneRelation[User]
+
+    @property
+    def locale(self) -> Optional[Locale]:
+        return Locale(self.lang) if self.lang else None

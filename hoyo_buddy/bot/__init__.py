bf1d3107f49d183dc7877436ce6725b627aa196a
seriaati <seria.ati@gmail.com>
2023-10-23 15:58:21 +0800

    feat: add logging when bot is shutting down and loaded cog

--- hoyo_buddy/bot/__init__.py
+++ hoyo_buddy/bot/__init__.py
@@ -30,11 +30,13 @@ class HoyoBuddy(commands.AutoShardedBot):
             cog_name = Path(filepath).stem
             try:
                 await self.load_extension(f"hoyo_buddy.cogs.{cog_name}")
+                log.info(f"Loaded cog {cog_name}")
             except Exception as e:  # skipcq: PYL-W0703
                 log.error(f"Failed to load cog {cog_name}: {e}", exc_info=True)
 
         await self.load_extension("jishaku")
 
     async def close(self):
+        log.info("Shutting down...")
         await self.translator.unload()
         await super().close()

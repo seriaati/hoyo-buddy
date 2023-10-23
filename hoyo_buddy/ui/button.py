54f8130d96a2607ac75fabef08615bb3b31ac41d
seriaati <seria.ati@gmail.com>
2023-10-23 20:55:22 +0800

    feat: add custom button class

---
+++ hoyo_buddy/ui/button.py
@@ -0,0 +1,52 @@
+from typing import Optional, Union
+
+import discord
+from discord.emoji import Emoji
+from discord.enums import ButtonStyle
+from discord.partial_emoji import PartialEmoji
+
+from ..bot import HoyoBuddy, emojis
+from ..bot.translator import Translator
+from ..db.models import User
+
+
+class Button(discord.ui.Button):
+    def __init__(
+        self,
+        *,
+        style: ButtonStyle = ButtonStyle.secondary,
+        label: Optional[str] = None,
+        disabled: bool = False,
+        custom_id: Optional[str] = None,
+        url: Optional[str] = None,
+        emoji: Optional[Union[Emoji, PartialEmoji, str]] = None,
+        row: Optional[int] = None
+    ):
+        super().__init__(
+            style=style,
+            label=label,
+            disabled=disabled,
+            custom_id=custom_id,
+            url=url,
+            emoji=emoji,
+            row=row,
+        )
+
+    async def translate(
+        self,
+        locale: discord.Locale,
+        translator: Translator,
+        *,
+        translate_label: bool = True,
+        **kwargs
+    ):
+        if self.label and translate_label:
+            self.label = await translator.translate(self.label, locale, **kwargs)
+
+    async def set_loading_state(self, i: discord.Interaction[HoyoBuddy]) -> None:
+        self.disabled = True
+        self.emoji = emojis.LOADING
+        self.label = "Loading..."
+        user = await User.get(id=i.user.id).prefetch_related("settings")
+        await self.translate(user.settings.locale or i.locale, i.client.translator)
+        await i.response.edit_message(view=self.view)

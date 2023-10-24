0a9aa59fdc0b33fa30f1b42d7fac21c05504abeb
seriaati <seria.ati@gmail.com>
2023-10-24 17:22:16 +0800

    feat: Add Admin cog with sync command

---
+++ hoyo_buddy/cogs/admin.py
@@ -0,0 +1,23 @@
+import asyncio
+import os
+from typing import Any
+
+from discord.app_commands import locale_str as _T
+from discord.ext import commands
+
+from ..bot import HoyoBuddy
+
+
+class Admin(commands.Cog):
+    def __init__(self, bot: HoyoBuddy):
+        self.bot = bot
+
+    @commands.command(name="sync")
+    async def sync_command(self, ctx: commands.Context) -> Any:
+        message = await ctx.send("Syncing commands...")
+        synced_commands = await self.bot.tree.sync()
+        await message.edit(content=f"Synced {len(synced_commands)} commands.")
+
+
+async def setup(bot: HoyoBuddy):
+    await bot.add_cog(Admin(bot))

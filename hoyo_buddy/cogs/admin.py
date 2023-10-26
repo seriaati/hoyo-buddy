6a05853f7ca824c88e1feecacdb2546af099dee6
seriaati <seria.ati@gmail.com>
2023-10-26 18:01:27 +0800

    feat: Add push-source-strings command to Admin cog

--- hoyo_buddy/cogs/admin.py
+++ hoyo_buddy/cogs/admin.py
@@ -16,6 +16,12 @@ class Admin(commands.Cog):
         synced_commands = await self.bot.tree.sync()
         await message.edit(content=f"Synced {len(synced_commands)} commands.")
 
+    @commands.command(name="push-source-strings", aliases=["pss"])
+    async def push_source_strings_command(self, ctx: commands.Context) -> Any:
+        message = await ctx.send("Pushing source strings...")
+        await self.bot.translator.push_source_strings()
+        await message.edit(content="Pushed source strings.")
+
 
 async def setup(bot: HoyoBuddy):
     await bot.add_cog(Admin(bot))

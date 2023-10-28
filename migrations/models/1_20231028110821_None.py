7abeb0bbd2dc53a9112051e678a110b27af8875f
seriaati <seria.ati@gmail.com>
2023-10-28 11:10:18 +0800

    feat: Add initial database migration and configuration

---
+++ migrations/models/1_20231028110821_None.py
@@ -0,0 +1,37 @@
+from tortoise.backends.base.client import BaseDBAsyncClient
+
+
+async def upgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        CREATE TABLE IF NOT EXISTS "user" (
+    "id" BIGINT NOT NULL  PRIMARY KEY,
+    "temp_data" JSON NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "hoyoaccount" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "uid" INT NOT NULL,
+    "username" VARCHAR(32) NOT NULL,
+    "nickname" VARCHAR(32),
+    "game" VARCHAR(32) NOT NULL  /* GENSHIN: Genshin Impact\nSTARRAIL: Honkai: Star Rail\nHONKAI: Honkai Impact 3rd */,
+    "cookies" TEXT NOT NULL,
+    "server" VARCHAR(32) NOT NULL,
+    "user_id" BIGINT NOT NULL REFERENCES "user" ("id") ON DELETE CASCADE,
+    CONSTRAINT "uid_hoyoaccount_uid_9c6d9f" UNIQUE ("uid", "game")
+);
+CREATE INDEX IF NOT EXISTS "idx_hoyoaccount_uid_e838aa" ON "hoyoaccount" ("uid");
+CREATE TABLE IF NOT EXISTS "settings" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "lang" VARCHAR(5),
+    "user_id" BIGINT NOT NULL UNIQUE REFERENCES "user" ("id") ON DELETE CASCADE
+);
+CREATE TABLE IF NOT EXISTS "aerich" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "version" VARCHAR(255) NOT NULL,
+    "app" VARCHAR(100) NOT NULL,
+    "content" JSON NOT NULL
+);"""
+
+
+async def downgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        """

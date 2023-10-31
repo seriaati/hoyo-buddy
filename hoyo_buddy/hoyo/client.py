08e48dee8450dd71d738ef0dc9a5164e11a8d986
seriaati <seria.ati@gmail.com>
2023-10-31 21:14:02 +0800

    feat: add daily reward embed func

--- hoyo_buddy/hoyo/client.py
+++ hoyo_buddy/hoyo/client.py
@@ -3,6 +3,11 @@ from typing import Optional
 import genshin
 from discord import Locale
 
+from ..bot.embeds import DefaultEmbed
+from ..bot.translator import Translator
+from ..bot.translator import locale_str as _T
+from ..db.enums import GAME_THUMBNAILS
+
 LOCALE_CONVERTER = {
     Locale.british_english: "en-us",
     Locale.american_english: "en-us",
@@ -34,3 +39,24 @@ class GenshinClient(genshin.Client):
 
     def set_lang(self, locale: Locale) -> None:
         self.lang = LOCALE_CONVERTER[locale]
+
+    def get_daily_reward_embed(
+        self,
+        daily_reward: genshin.models.DailyReward,
+        game: genshin.Game,
+        locale: Locale,
+        translator: Translator,
+    ) -> DefaultEmbed:
+        embed = DefaultEmbed(
+            locale,
+            translator,
+            title=_T("Reward claimed", key="reward_claimed_title"),
+            description=_T(
+                f"{daily_reward.name} x{daily_reward.amount}", translate=False
+            ),
+        )
+        embed.set_thumbnail(url=daily_reward.icon)
+        embed.set_author(
+            name=_T(game.value, warn_no_key=False), icon_url=GAME_THUMBNAILS[game]
+        )
+        return embed

6c3bfdcf1af7b9968143295aff110a26530a08aa
seriaati <seria.ati@gmail.com>
2023-10-31 20:55:53 +0800

    feat: upload font files

---
+++ hoyo_buddy/draw/fonts/__init__.py
@@ -0,0 +1,47 @@
+from PIL import ImageFont
+
+__all__ = (
+    "NUNITO_LIGHT",
+    "NUNITO_REGULAR",
+    "NUNITO_MEDIUM",
+    "NUNITO_BOLD",
+    "MPLUSROUNDED1C_LIGHT",
+    "MPLUSROUNDED1C_REGULAR",
+    "MPLUSROUNDED1C_MEDIUM",
+    "MPLUSROUNDED1C_BOLD",
+    "GENSENROUNDEDTW_LIGHT",
+    "GENSENROUNDEDTW_REGULAR",
+    "GENSENROUNDEDTW_MEDIUM",
+    "GENSENROUNDEDTW_BOLD",
+)
+
+NUNITO_LIGHT = ImageFont.truetype("hoyo_buddy/draw/fonts/Nunito-Light.ttf", 20)
+NUNITO_REGULAR = ImageFont.truetype("hoyo_buddy/draw/fonts/Nunito-Regular.ttf", 20)
+NUNITO_MEDIUM = ImageFont.truetype("hoyo_buddy/draw/fonts/Nunito-SemiBold.ttf", 20)
+NUNITO_BOLD = ImageFont.truetype("hoyo_buddy/draw/fonts/Nunito-Bold.ttf", 20)
+
+MPLUSROUNDED1C_LIGHT = ImageFont.truetype(
+    "hoyo_buddy/draw/fonts/MPLUSRounded1c-Light.ttf", 20
+)
+MPLUSROUNDED1C_REGULAR = ImageFont.truetype(
+    "hoyo_buddy/draw/fonts/MPLUSRounded1c-Regular.ttf", 20
+)
+MPLUSROUNDED1C_MEDIUM = ImageFont.truetype(
+    "hoyo_buddy/draw/fonts/MPLUSRounded1c-Medium.ttf", 20
+)
+MPLUSROUNDED1C_BOLD = ImageFont.truetype(
+    "hoyo_buddy/draw/fonts/MPLUSRounded1c-Bold.ttf", 20
+)
+
+GENSENROUNDEDTW_LIGHT = ImageFont.truetype(
+    "hoyo_buddy/draw/fonts/GenSenRoundedTW-L-01.ttf", 20
+)
+GENSENROUNDEDTW_REGULAR = ImageFont.truetype(
+    "hoyo_buddy/draw/fonts/GenSenRoundedTW-R-01.ttf", 20
+)
+GENSENROUNDEDTW_MEDIUM = ImageFont.truetype(
+    "hoyo_buddy/draw/fonts/GenSenRoundedTW-M-01.ttf", 20
+)
+GENSENROUNDEDTW_BOLD = ImageFont.truetype(
+    "hoyo_buddy/draw/fonts/GenSenRoundedTW-B-01.ttf", 20
+)

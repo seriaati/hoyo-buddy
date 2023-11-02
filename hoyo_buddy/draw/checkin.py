6a72762713096ca817d5bdd28ef6bf39e754974c
seriaati <seria.ati@gmail.com>
2023-11-02 20:05:28 +0800

    feat: check-in image drawing

--- hoyo_buddy/draw/checkin.py
+++ hoyo_buddy/draw/checkin.py
@@ -1,18 +1,84 @@
-import discord
-from PIL import Image
+import io
+from typing import Tuple
 
-from ..bot.translator import Translator
+import genshin
+from cachetools import LRUCache, cached
+from PIL import Image, ImageDraw
 
-LIGHT_1 = Image.open("hoyo-buddy-assets/assets/check-in/LIGHT_1.png")
-LIGHT_2 = Image.open("hoyo-buddy-assets/assets/check-in/LIGHT_2.png")
-LIGHT_CHECK = Image.open("hoyo-buddy-assets/assets/check-in/LIGHT_CHECK.png")
-LIGHT_MASK = Image.open("hoyo-buddy-assets/assets/check-in/LIGHT_MASK.png")
-
-DARK_1 = Image.open("hoyo-buddy-assets/assets/check-in/DARK_1.png")
-DARK_2 = Image.open("hoyo-buddy-assets/assets/check-in/DARK_2.png")
-DARK_CHECK = Image.open("hoyo-buddy-assets/assets/check-in/DARK_CHECK.png")
-DARK_MASK = Image.open("hoyo-buddy-assets/assets/check-in/DARK_MASK.png")
+from . import Drawer
 
 
-def draw(locale: discord.Locale, translator: Translator, dark_mode: bool):
-    pass
+def cache_key(
+    daily_rewards: Tuple[genshin.models.DailyReward, ...], dark_mode: bool
+) -> str:
+    rewards_key = "_".join(
+        f"{daily_reward.name}_{daily_reward.amount}" for daily_reward in daily_rewards
+    )
+    return f"{rewards_key}_{dark_mode}"
+
+
+@cached(cache=LRUCache(maxsize=100), key=cache_key)
+def draw(
+    daily_rewards: Tuple[genshin.models.DailyReward, ...],
+    dark_mode: bool,
+) -> io.BytesIO:
+    if dark_mode:
+        im = Image.open("hoyo-buddy-assets/assets/check-in/DARK_1.png")
+        check = Image.open("hoyo-buddy-assets/assets/check-in/DARK_CHECK.png")
+        mask = Image.open("hoyo-buddy-assets/assets/check-in/DARK_MASK.png")
+    else:
+        im = Image.open("hoyo-buddy-assets/assets/check-in/LIGHT_1.png")
+        check = Image.open("hoyo-buddy-assets/assets/check-in/LIGHT_CHECK.png")
+        mask = Image.open("hoyo-buddy-assets/assets/check-in/LIGHT_MASK.png")
+
+    draw = ImageDraw.Draw(im)
+    drawer = Drawer(draw, folder="check-in")
+
+    x, y = (44, 36)
+    for i, daily_reward in enumerate(daily_rewards):
+        icon = drawer.get_static_image(daily_reward.icon)
+        icon = icon.resize((110, 110))
+        im.paste(icon, (x, y), icon)
+        try:
+            status, index = daily_reward.name.split("_")
+        except ValueError:
+            status = "unclaimed"
+            index = daily_reward.name
+
+        if status == "claimed":
+            im.paste(mask, (x - 19, y - 11), mask)
+            im.paste(check, (x + 1, y + 1), check)
+
+        color = "#BEBEBE" if dark_mode else "#8A8B97"
+        if i in (2, 3):
+            color = "#FFFFFF" if dark_mode else "#282B3C"
+        drawer.plain_write(
+            text=f"x{daily_reward.amount}",
+            size=36,
+            color=color,
+            position=(x + 56, y + 153),
+            style="medium" if i in (2, 3) else "regular",
+            anchor="mm",
+        )
+
+        color = "#BEBEBE" if dark_mode else "#6A6C7C"
+        if i in (2, 3):
+            color = "#FFFFFF" if dark_mode else "#282B3C"
+        drawer.plain_write(
+            text=index,
+            size=18,
+            color=color,
+            position=(x + 55, y + 195),
+            style="regular" if i in (2, 3) else "light",
+            anchor="mm",
+        )
+
+        if i == 2:
+            x += 166 + icon.width
+        else:
+            x += 64 + icon.width
+
+    bytes_io = io.BytesIO()
+    im.save(bytes_io, format="PNG")
+
+    return bytes_io

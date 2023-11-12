63d799dee362133a6e773c0768395eb5a3319a3a
seriaati <seria.ati@gmail.com>
2023-11-13 05:51:58 +0800

    feat: add InvalidInput exception

--- hoyo_buddy/exceptions.py
+++ hoyo_buddy/exceptions.py
@@ -5,3 +5,11 @@ class HoyoBuddyError(Exception):
     def __init__(self, title: _T, message: _T):
         self.title = title
         self.message = message
+
+
+class InvalidInput(HoyoBuddyError):
+    def __init__(self, reason: _T):
+        super().__init__(
+            title=_T("Invalid input", key="invalid_input_error_title"),
+            message=reason,
+        )

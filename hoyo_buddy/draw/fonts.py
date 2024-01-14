6a22316b4b9fac6bf61a4a9f316e723e02e39584
seria <seria.ati@gmail.com>
2024-01-14 11:29:00 +0800

    chore: Organize assets

---
+++ hoyo_buddy/draw/fonts.py
@@ -0,0 +1,14 @@
+NUNITO_LIGHT = "hoyo-buddy-assets/fonts/Nunito/Nunito-Light.ttf"
+NUNITO_REGULAR = "hoyo-buddy-assets/fonts/Nunito/Nunito-Regular.ttf"
+NUNITO_MEDIUM = "hoyo-buddy-assets/fonts/Nunito/Nunito-SemiBold.ttf"
+NUNITO_BOLD = "hoyo-buddy-assets/fonts/Nunito/Nunito-Bold.ttf"
+
+MPLUSROUNDED1C_LIGHT = "hoyo-buddy-assets/fonts/MPLUSRounded1c/MPLUSRounded1c-Light.ttf"
+MPLUSROUNDED1C_REGULAR = "hoyo-buddy-assets/fonts/MPLUSRounded1c/MPLUSRounded1c-Regular.ttf"
+MPLUSROUNDED1C_MEDIUM = "hoyo-buddy-assets/fonts/MPLUSRounded1c/MPLUSRounded1c-Medium.ttf"
+MPLUSROUNDED1C_BOLD = "hoyo-buddy-assets/fonts/MPLUSRounded1c/MPLUSRounded1c-Bold.ttf"
+
+GENSENROUNDEDTW_LIGHT = "hoyo-buddy-assets/fonts/GenSenRounded/GenSenRoundedTW-L-01.ttf"
+GENSENROUNDEDTW_REGULAR = "hoyo-buddy-assets/fonts/GenSenRounded/GenSenRoundedTW-R-01.ttf"
+GENSENROUNDEDTW_MEDIUM = "hoyo-buddy-assets/fonts/GenSenRounded/GenSenRoundedTW-M-01.ttf"
+GENSENROUNDEDTW_BOLD = "hoyo-buddy-assets/fonts/GenSenRounded/GenSenRoundedTW-B-01.ttf"

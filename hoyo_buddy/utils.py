2edcdcbf0e459613f514a63585ba5f29e9273d5c
seria <seria.ati@gmail.com>
2024-02-27 17:00:30 +0800

    feat: Add new utils

--- hoyo_buddy/utils.py
+++ hoyo_buddy/utils.py
@@ -1,13 +1,18 @@
 import datetime
 import logging
+import re
 import time
 from typing import TYPE_CHECKING, Any, TypeVar
+
+import aiohttp
 
 if TYPE_CHECKING:
     from collections.abc import Callable
 
 
 T = TypeVar("T")
+
+IMAGE_EXTENSIONS = (".jpg", ".jpeg", ".png", ".webp")
 
 LOGGER_ = logging.getLogger(__name__)
 
@@ -31,3 +36,28 @@ def timer(func: "Callable[..., Any]") -> "Callable[..., Any]":
         return result
 
     return wrapper
+
+
+async def test_url_validity(url: str, session: aiohttp.ClientSession) -> bool:
+    """
+    Test if a URL is valid by sending a HEAD request
+    """
+    try:
+        async with session.head(url) as resp:
+            return resp.status == 200
+    except aiohttp.ClientError:
+        return False
+
+
+def is_image_url(url: str) -> bool:
+    """
+    Test if a URL is an image URL
+    """
+    return url.endswith(IMAGE_EXTENSIONS)
+
+
+def is_valid_hex_color(color: str) -> bool:
+    """
+    Test if a string is a valid hex color
+    """
+    return bool(re.match(r"^#(?:[0-9a-fA-F]{3}){1,2}$", color))

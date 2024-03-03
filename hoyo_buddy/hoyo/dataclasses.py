a0b030515ff62929d23e90035316a6488d370c82
seria <seria.ati@gmail.com>
2024-03-03 14:02:47 +0800

    refactor: Remove custom response dataclasses

--- hoyo_buddy/hoyo/dataclasses.py
+++ hoyo_buddy/hoyo/dataclasses.py
@@ -1,12 +1,4 @@
-from typing import TYPE_CHECKING
-
-import mihomo
 from attr import dataclass
-
-from ..constants import MIHOMO_LANG_TO_LOCALE
-
-if TYPE_CHECKING:
-    import discord
 
 
 @dataclass(kw_only=True)
@@ -16,16 +8,3 @@ class Reward:
     index: int
     claimed: bool
     icon: str
-
-
-class ExtendedCharacter(mihomo.models.Character):
-    live: bool = False
-    lang: str = "en"
-
-    @property
-    def locale(self) -> "discord.Locale":
-        return MIHOMO_LANG_TO_LOCALE[mihomo.Language(self.lang)]
-
-
-class ExtendedStarRailInfoParsed(mihomo.StarrailInfoParsed):
-    characters: list[ExtendedCharacter]

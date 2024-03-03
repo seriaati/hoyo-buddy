024f4cc0ca00107b1b228a789d197332569ff31f
seria <seria.ati@gmail.com>
2024-03-03 14:02:27 +0800

    refactor(mihomo): Modify cache mechanism

--- hoyo_buddy/hoyo/mihomo_client.py
+++ hoyo_buddy/hoyo/mihomo_client.py
@@ -1,14 +1,15 @@
 import logging
+import pickle
 from typing import TYPE_CHECKING
 
 import mihomo
 
 from ..constants import LOCALE_TO_MIHOMO_LANG
 from ..db.models import EnkaCache
-from .dataclasses import ExtendedStarRailInfoParsed
 
 if TYPE_CHECKING:
     import discord
+    from mihomo.models import Character, StarrailInfoParsed
 
 LOGGER_ = logging.getLogger(__name__)
 
@@ -18,51 +19,49 @@ class MihomoAPI(mihomo.MihomoAPI):
         super().__init__(LOCALE_TO_MIHOMO_LANG.get(language, mihomo.Language.EN))
 
     def _update_cache_with_live_data(
-        self, cache: EnkaCache, live_data: mihomo.StarrailInfoParsed
+        self, cache: EnkaCache, live_data: "StarrailInfoParsed"
     ) -> None:
-        cache_player = cache.hsr.get("player", {})
-        cache_player.update(live_data.player.model_dump(by_alias=True))
-        cache.hsr["player"] = cache_player
+        if cache.hsr is None:
+            cache.hsr = pickle.dumps(live_data)
+            cache.extras.update(
+                {
+                    str(char.id): {"live": True, "lang": self.lang.value}
+                    for char in live_data.characters
+                }
+            )
+            return
 
-        cache_characters = cache.hsr.get("characters", [])
+        cache_data: StarrailInfoParsed = pickle.loads(cache.hsr)
+
+        live_character_ids: list[str] = []
         for character in live_data.characters:
-            model_dump = character.model_dump(by_alias=True)
-            model_dump["lang"] = self.lang.value
+            live_character_ids.append(character.id)
+            cache.extras.get(str(character.id), {}).update({"live": True, "lang": self.lang.value})
 
-            for c in cache_characters:
-                if c["id"] == character.id:
-                    c.update(model_dump)
-                    break
-            else:
-                cache_characters.append(model_dump)
-        cache.hsr["characters"] = cache_characters
+        cache_characters_not_in_live: list["Character"] = []
 
-    async def fetch_user(self, uid: int) -> ExtendedStarRailInfoParsed:
+        for character in cache_data.characters:
+            if character.id not in live_character_ids:
+                cache_characters_not_in_live.append(character)
+                cache.extras[str(character.id)].update({"live": False})
+
+        cache_data.characters = cache_characters_not_in_live + live_data.characters
+        cache.hsr = pickle.dumps(cache_data)
+
+    async def fetch_user(self, uid: int) -> "StarrailInfoParsed":
         cache, _ = await EnkaCache.get_or_create(uid=uid)
-        live_data_character_ids: list[str] = []
 
         try:
             live_data = await super().fetch_user(uid, replace_icon_name_with_url=True)
         except mihomo.UserNotFound:
             if not cache.hsr:
                 raise
-            cache_data = ExtendedStarRailInfoParsed(**cache.hsr)
+            cache_data: StarrailInfoParsed = pickle.loads(cache.hsr)
         else:
-            live_data_character_ids.extend([char.id for char in live_data.characters])
             self._update_cache_with_live_data(cache, live_data)
             await cache.save()
+            assert cache.hsr is not None
 
-            cache_data = ExtendedStarRailInfoParsed(**cache.hsr)
-
-        for character in cache_data.characters:
-            character.live = character.id in live_data_character_ids
-            character.lang = next(
-                (
-                    c.get("lang", self.lang.value)
-                    for c in cache.hsr["characters"]
-                    if c["id"] == character.id
-                ),
-                self.lang.value,
-            )
+            cache_data: StarrailInfoParsed = pickle.loads(cache.hsr)
 
         return cache_data

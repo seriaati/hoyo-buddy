0a2bbccc04e294202db6fb96e3610527bb2e0bdb
seria <seria.ati@gmail.com>
2024-03-06 23:30:08 +0800

    refactor: Improve description in adding acc

--- src/ui/account/items/add_acc_btn.py
+++ src/ui/account/items/add_acc_btn.py
@@ -32,15 +32,16 @@ class AddAccountButton(Button["AccountManager"]):
             self.view.translator,
             title=LocaleStr("Adding accounts", key="adding_accounts_title"),
             description=LocaleStr(
-                "Below are 3 ways you can add accounts; however, it is recommended to try the first one, then work your way through the others if it doesn't work.",
+                "Select one of the methods below to add your accounts to Hoyo Buddy",
                 key="adding_accounts_description",
             ),
         )
         go_back_button = GoBackButton(self.view.children, self.view.get_embeds(i.message))
         self.view.clear_items()
-        self.view.add_item(WithJavaScript())
-        self.view.add_item(WithDevTools())
+
         self.view.add_item(WithEmailPassword())
+        self.view.add_item(WithDevTools())
+        self.view.add_item(WithJavaScript())
         self.view.add_item(go_back_button)
 
         await i.response.edit_message(embed=embed, view=self.view)

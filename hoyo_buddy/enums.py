73d7026b22be0d18eb5122dd767c382e10813f83
seria <seria.ati@gmail.com>
2024-03-12 07:18:51 +0800

    refactor: Move game thumbnails to icons.py
    Add get_game_icon method

--- hoyo_buddy/enums.py
+++ hoyo_buddy/enums.py
@@ -2,8 +2,6 @@ from enum import IntEnum, StrEnum
 
 import genshin
 
-__all__ = ("Game", "GAME_CONVERTER", "GAME_THUMBNAILS")
-
 
 class Game(StrEnum):
     GENSHIN = "Genshin Impact"
@@ -53,13 +51,4 @@ GAME_CONVERTER: dict[Game | genshin.Game, Game | genshin.Game] = {
     genshin.Game.GENSHIN: Game.GENSHIN,
     genshin.Game.STARRAIL: Game.STARRAIL,
     genshin.Game.HONKAI: Game.HONKAI,
-}
-
-GAME_THUMBNAILS: dict[Game | genshin.Game, str] = {
-    Game.GENSHIN: "https://i.imgur.com/t0Y5tYb.png",
-    Game.STARRAIL: "https://i.imgur.com/nokmKT3.png",
-    Game.HONKAI: "https://i.imgur.com/8yJ4nWP.png",
-    genshin.Game.GENSHIN: "https://i.imgur.com/t0Y5tYb.png",
-    genshin.Game.STARRAIL: "https://i.imgur.com/nokmKT3.png",
-    genshin.Game.HONKAI: "https://i.imgur.com/8yJ4nWP.png",
 }
--- hoyo_buddy/icons.py
+++ hoyo_buddy/icons.py
@@ -1,6 +1,27 @@
+from genshin import Game
+
+from .enums import Game as GameEnum
+
 RESIN_ICON = "https://api.ambr.top/assets/UI/UI_ItemIcon_210.png"
 TBP_ICON = "https://api.yatta.top/hsr/assets/UI/item/11.png"
 RTBP_ICON = "https://api.yatta.top/hsr/assets/UI/item/12.png"
 PT_ICON = "https://api.ambr.top/assets/UI/UI_ItemIcon_220021.png"
 COMMISSION_ICON = "https://i.imgur.com/x73cn2G.png"
 REALM_CURRENCY_ICON = "https://i.imgur.com/eAJ0RFr.png"
+
+GI_ICON = "https://i.imgur.com/QqqrBOg.png"
+HSR_ICON = "https://i.imgur.com/jHyDVSv.png"
+HONKAI_ICON = "https://i.imgur.com/9ueIKNO.png"
+
+
+def get_game_icon(game: Game | GameEnum) -> str:
+    match game:
+        case Game.GENSHIN | GameEnum.GENSHIN:
+            return GI_ICON
+        case Game.STARRAIL | GameEnum.STARRAIL:
+            return HSR_ICON
+        case Game.HONKAI | GameEnum.HONKAI:
+            return HONKAI_ICON
+        case _:
+            msg = f"Invalid game: {game!r}"
+            raise ValueError(msg)

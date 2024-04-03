73f7c6ced12ae7564f22d134a6abce3410db4f99
seria <seria.ati@gmail.com>
2024-04-03 10:21:09 +0900

    fix: Remove src2 template
    Fixes HOYO-BUDDY-V

--- hoyo_buddy/ui/hoyo/profile/items/card_template_select.py
+++ hoyo_buddy/ui/hoyo/profile/items/card_template_select.py
@@ -16,7 +16,7 @@ if TYPE_CHECKING:
 class CardTemplateSelect(Select["ProfileView"]):
     def __init__(self, current_template: str, hb_only: bool, game: Game) -> None:
         hb_templates = (1,)
-        src_templates = (1, 2, 3)
+        src_templates = (1, 3)
         enkac_templates = (1, 2, 3, 5, 7)  # EnkaCard2 templates
 
         options: list[SelectOption] = []

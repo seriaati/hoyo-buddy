b1fff3db95ef22ec0110dfd63c28a182717ab698
seria <seria.ati@gmail.com>
2024-04-10 11:49:27 +0900

    chore(db): Update migration files

---
+++ migrations/models/8_20240410072420_update.py
@@ -0,0 +1,10 @@
+async def upgrade(db) -> str:
+    return """
+        ALTER TABLE "hoyoaccount" ADD "redeemed_codes" JSONB NOT NULL DEFAULT '[]';
+        ALTER TABLE "hoyoaccount" ADD "auto_redeem" BOOL NOT NULL  DEFAULT True;"""
+
+
+async def downgrade(db) -> str:
+    return """
+        ALTER TABLE "hoyoaccount" DROP COLUMN "redeemed_codes";
+        ALTER TABLE "hoyoaccount" DROP COLUMN "auto_redeem";"""

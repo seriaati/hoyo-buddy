2acacedae10e8add6c40036e3c510ca6ee8a3a33
seriaati <seria.ati@gmail.com>
2024-05-08 23:32:55 +0800

    refactor: Refactor static image downloader

--- hoyo_buddy/draw/static.py
+++ hoyo_buddy/draw/static.py
@@ -1,8 +1,11 @@
+from __future__ import annotations
+
 import asyncio
-import os
+import pathlib
 from typing import TYPE_CHECKING
 
 import aiofiles
+from seria.utils import clean_url
 
 from ..exceptions import DownloadImageFailedError
 
@@ -11,48 +14,45 @@ if TYPE_CHECKING:
 
     import aiohttp
 
-__all__ = ("STATIC_FOLDER", "download_and_save_static_images")
 
-STATIC_FOLDER = "./.static"
+STATIC_FOLDER = pathlib.Path("./.static")
 
 
-async def download_static_image(image_url: str, session: "aiohttp.ClientSession") -> bytes:
+async def download_img(image_url: str, session: aiohttp.ClientSession) -> bytes:
     async with session.get(image_url) as resp:
         if resp.status != 200:
             raise DownloadImageFailedError(image_url, resp.status)
+
         image = await resp.read()
         return image
 
 
-async def save_static_image(folder: str, filename: str, image: bytes) -> None:
-    if not os.path.exists(STATIC_FOLDER):
-        os.makedirs(STATIC_FOLDER)
-    if not os.path.exists(f"{STATIC_FOLDER}/{folder}"):
-        os.makedirs(f"{STATIC_FOLDER}/{folder}")
+async def save_img(folder: str, filename: str, image: bytes) -> None:
+    folder_path = STATIC_FOLDER / folder
+    if not folder_path.exists():
+        folder_path.mkdir(parents=True)
 
-    async with aiofiles.open(f"{STATIC_FOLDER}/{folder}/{filename}", "wb") as f:
+    async with aiofiles.open(folder_path / filename, "wb") as f:
         await f.write(image)
 
 
-async def download_and_save_static_image_task(
-    image_url: str, folder: str, filename: str, session: "aiohttp.ClientSession"
+async def download_and_save_img(
+    image_url: str, folder: str, filename: str, session: aiohttp.ClientSession
 ) -> None:
-    image = await download_static_image(image_url, session)
-    await save_static_image(folder, filename, image)
+    image = await download_img(image_url, session)
+    await save_img(folder, filename, image)
 
 
 async def download_and_save_static_images(
-    image_urls: "Sequence[str]", folder: str, session: "aiohttp.ClientSession"
+    image_urls: Sequence[str], folder: str, session: aiohttp.ClientSession
 ) -> None:
-    tasks: "list[asyncio.Task]" = []
     image_urls = list(set(image_urls))
+
+    tasks = []
     for image_url in image_urls:
-        filename = image_url.split("/")[-1]
-        if not os.path.exists(f"{STATIC_FOLDER}/{folder}/{filename}"):
-            tasks.append(
-                asyncio.create_task(
-                    download_and_save_static_image_task(image_url, folder, filename, session)
-                )
-            )
+        filename = clean_url(image_url).split("/")[-1]
+        file_path = STATIC_FOLDER / folder / filename
+        if not file_path.exists():
+            tasks.append(download_and_save_img(image_url, folder, filename, session))
 
     await asyncio.gather(*tasks)

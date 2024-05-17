4d647434cb6c05eb775451760dababad5a801a25
seria <seria.ati@gmail.com>
2024-05-18 08:19:07 +0900

    chore(migrations): Add back deleted migration file

---
+++ migrations/models/10_20240502073030_update.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+from tortoise import BaseDBAsyncClient
+
+
+async def upgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        ALTER TABLE "hoyoaccount" ALTER COLUMN "uid" TYPE BIGINT USING "uid"::BIGINT;"""
+
+
+async def downgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        ALTER TABLE "hoyoaccount" ALTER COLUMN "uid" TYPE INT USING "uid"::INT;"""

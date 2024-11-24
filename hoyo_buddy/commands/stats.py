224e291b6736d974a8cb16d355e9ce3857bd6b27
seria <seria.ati@gmail.com>
2024-11-24 09:37:10 +0800

    feat: Add more stats to /stats

--- hoyo_buddy/commands/stats.py
+++ hoyo_buddy/commands/stats.py
@@ -2,15 +2,14 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from hoyo_buddy.enums import Game
 from hoyo_buddy.exceptions import NoAccountFoundError
 from hoyo_buddy.utils import ephemeral
 
-from ..db.models import HoyoAccount, get_dyk, get_locale
+from ..db.models import get_locale
 from ..ui.hoyo.stats import StatsView
 
 if TYPE_CHECKING:
-    import genshin
-
     from ..types import Interaction, User
 
 
@@ -23,32 +22,11 @@ class StatsCommand:
         locale = await get_locale(i)
 
         user = self._user or i.user
-        accounts = await HoyoAccount.filter(user_id=user.id).all()
+        accounts = await i.client.get_accounts(
+            user.id, games=(Game.GENSHIN, Game.STARRAIL, Game.ZZZ)
+        )
         if not accounts:
             raise NoAccountFoundError
 
-        uids = {account.uid for account in accounts}
-        record_cards: dict[int, genshin.models.RecordCard] = {}
-
-        for account in accounts:
-            if account.uid in record_cards:
-                continue
-
-            client = account.client
-            client.set_lang(locale)
-            cards = await client.get_record_cards()
-
-            for card in cards:
-                if card.uid not in uids:
-                    continue
-                record_cards[card.uid] = card
-
-        record_cards_ = list(record_cards.values())
-        if not record_cards_:
-            raise NoAccountFoundError
-
-        view = StatsView(record_cards_, author=i.user, locale=locale)
-        await i.followup.send(
-            embed=view.get_card_embed(record_cards_[0]), view=view, content=await get_dyk(i)
-        )
-        view.message = await i.original_response()
+        view = StatsView(accounts, author=i.user, locale=locale)
+        await view.start(i)
--- hoyo_buddy/ui/hoyo/stats.py
+++ hoyo_buddy/ui/hoyo/stats.py
@@ -1,16 +1,20 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
+
+from hoyo_buddy.db.models import HoyoAccount, get_dyk
+from hoyo_buddy.enums import Game
+from hoyo_buddy.exceptions import FeatureNotImplementedError
+from hoyo_buddy.icons import get_game_icon
 
 from ...embeds import DefaultEmbed
 from ...emojis import get_game_emoji
-from ...l10n import LevelStr, LocaleStr
+from ...l10n import EnumStr, LocaleStr
 from ...utils import blur_uid
 from ..components import Select, SelectOption, View
 
 if TYPE_CHECKING:
-    from collections.abc import Sequence
-
+    import genshin
     from discord import Locale
     from genshin.models import RecordCard
 
@@ -26,37 +30,124 @@ def get_label(card: RecordCard) -> str:
 
 
 class StatsView(View):
-    def __init__(self, record_cards: Sequence[RecordCard], *, author: User, locale: Locale) -> None:
+    def __init__(self, accounts: list[HoyoAccount], *, author: User, locale: Locale) -> None:
         super().__init__(author=author, locale=locale)
-        self._record_cards = record_cards
-        self.add_item(CardSelect(record_cards))
+        self.accounts = accounts
+        self.account = accounts[0]
+        self.add_item(AccountSwitcher(accounts, self.account))
 
-    def get_card(self, uid: int) -> RecordCard:
-        return next(card for card in self._record_cards if card.uid == uid)
+    def _get_user_embed(self, *, level: int, fields: dict[str, Any], avatar: str) -> DefaultEmbed:
+        embed = DefaultEmbed(
+            self.locale,
+            title=self.account.blurred_display,
+            description=LocaleStr(key="N_level", mi18n_game=Game.GENSHIN, N=level),
+        )
+        for key, value in fields.items():
+            embed.add_field(name=LocaleStr(key=key, mi18n_game=self.account.game), value=str(value))
 
-    def get_card_embed(self, card: RecordCard) -> DefaultEmbed:
-        embed = DefaultEmbed(self.locale, title=get_label(card), description=LevelStr(card.level))
-        for d in card.data:
-            embed.add_field(name=d.name, value=d.value)
-        embed.set_author(name=card.game_name, icon_url=card.game_logo)
+        embed.set_thumbnail(url=avatar)
+        embed.set_author(name=EnumStr(self.account.game), icon_url=get_game_icon(self.account.game))
         return embed
 
+    def get_genshin_user_embed(
+        self, genshin_user: genshin.models.PartialGenshinUserStats
+    ) -> DefaultEmbed:
+        stats = genshin_user.stats
+        fields = {
+            "active_day": stats.days_active,
+            "achievement_complete_count": stats.achievements,
+            "card_avatar_number": stats.characters,
+            "card_spriral_abyss": stats.spiral_abyss,
+            "full_fetter_avatar_num": stats.max_friendship_characters,
+            "unlock_portal": stats.unlocked_waypoints,
+            "unlock_secret_area": stats.unlocked_domains,
+            "wind_god": stats.anemoculi,
+            "geoculus": stats.geoculi,
+            "electroculus": stats.electroculi,
+            "dendroculus": stats.dendroculi,
+            "hydro_god": stats.hydroculi,
+            "pyroculus_number": stats.pyroculi,
+            "magnificent_treasure_box_count": stats.luxurious_chests,
+            "rarity_treasure_box_count": stats.precious_chests,
+            "delicacy_treasure_box_count": stats.exquisite_chests,
+            "general_treasure_box_count": stats.common_chests,
+            "magic_chest_number": stats.remarkable_chests,
+        }
+        return self._get_user_embed(
+            level=genshin_user.info.level, fields=fields, avatar=genshin_user.info.in_game_avatar
+        )
 
-class CardSelect(Select[StatsView]):
-    def __init__(self, record_cards: Sequence[RecordCard]) -> None:
-        options = [
-            SelectOption(
-                label=get_label(card),
-                value=str(card.uid),
-                emoji=get_game_emoji(card.game),
-                description=card.game_name,
-                default=card.uid == record_cards[0].uid,
-            )
-            for card in record_cards
-        ]
-        super().__init__(options=options, placeholder=LocaleStr(key="stats.select_card"))
+    def get_hsr_user_embed(self, user: genshin.models.StarRailUserStats) -> DefaultEmbed:
+        stats = user.stats
+        fields = {
+            "hsr_active_day": stats.active_days,
+            "hsr_get_account_count": stats.avatar_num,
+            "achieve_total_text": stats.achievement_num,
+            "hsr_all_chest_count": stats.chest_num,
+            "hsr_dream_paster_num": stats.dreamscape_pass_sticker,
+        }
+        return self._get_user_embed(
+            level=user.info.level, fields=fields, avatar=user.in_game_avatar
+        )
+
+    def get_zzz_user_embed(
+        self, user: genshin.models.ZZZUserStats, card: genshin.models.RecordCard
+    ) -> DefaultEmbed:
+        stats = user.stats
+        fields = {
+            "active_days": stats.active_days,
+            "avatar_num": stats.character_num,
+            "buddy_num": stats.bangboo_obtained,
+            "cur_period_zone_layer_count": stats.shiyu_defense_frontiers,
+            "achievement_count": stats.achievement_count,
+            "commemorative_coins_list": stats.hia_coin.num,
+        }
+        return self._get_user_embed(level=card.level, fields=fields, avatar=user.in_game_avatar)
+
+    async def start(self, i: Interaction, *, acc_select: AccountSwitcher | None = None) -> None:
+        client = self.account.client
+        client.set_lang(self.locale)
+        if self.account.game is Game.GENSHIN:
+            user = await client.get_partial_genshin_user(self.account.uid)
+            embed = self.get_genshin_user_embed(user)
+        elif self.account.game is Game.STARRAIL:
+            user = await client.get_starrail_user(self.account.uid)
+            embed = self.get_hsr_user_embed(user)
+        elif self.account.game is Game.ZZZ:
+            record_cards = await client.get_record_cards()
+            card = next(card for card in record_cards if card.uid == self.account.uid)
+            user = await client.get_zzz_user(self.account.uid)
+            embed = self.get_zzz_user_embed(user, card)
+        else:
+            raise FeatureNotImplementedError(game=self.account.game)
+
+        if acc_select is not None:
+            await acc_select.unset_loading_state(i, embed=embed)
+        await i.followup.send(embed=embed, view=self, content=await get_dyk(i))
+
+        self.message = await i.original_response()
+
+
+class AccountSwitcher(Select[StatsView]):
+    def __init__(self, accounts: list[HoyoAccount], account: HoyoAccount) -> None:
+        super().__init__(
+            placeholder=LocaleStr(key="account_select_placeholder"),
+            options=[
+                SelectOption(
+                    label=str(acc),
+                    value=f"{acc.uid}_{acc.game}",
+                    emoji=get_game_emoji(acc.game),
+                    default=acc == account,
+                )
+                for acc in accounts
+            ],
+        )
 
     async def callback(self, i: Interaction) -> None:
-        card = self.view.get_card(int(self.values[0]))
-        self.update_options_defaults()
-        await i.response.edit_message(embed=self.view.get_card_embed(card), view=self.view)
+        await self.set_loading_state(i)
+        uid, game = self.values[0].split("_")
+        account = next(
+            acc for acc in self.view.accounts if acc.uid == int(uid) and acc.game == game
+        )
+        self.view.account = account
+        await self.view.start(i, acc_select=self)

79642db41f3294250de04734900d79dcaba5e6e2
seria <seria.ati@gmail.com>
2025-02-15 23:17:12 +0800

    refactor: Update Prometheus metrics and improve command handling

--- hoyo_buddy/cogs/prometheus.py
+++ hoyo_buddy/cogs/prometheus.py
@@ -5,136 +5,134 @@ and https://github.com/KT-Yeh/Genshin-Discord-Bot/blob/2ff43ff5a4092afb3a8112d1a
 
 from __future__ import annotations
 
-from typing import Final
+from typing import TYPE_CHECKING, Final
 
 import psutil
-from discord import AutoShardedClient, Guild, Interaction, InteractionType
+from discord import Guild, Interaction, InteractionType, app_commands
 from discord.ext import commands, tasks
+from loguru import logger
 from prometheus_client import Counter, Gauge
 
-from hoyo_buddy.db.models import User
+if TYPE_CHECKING:
+    from hoyo_buddy.bot.bot import HoyoBuddy
 
 
 class Metrics:
     """定義各項用來傳送給 Prometheus Server 的 Metric"""
 
     PREFIX: Final[str] = "discordbot_"
-    """Metric 名字的前綴"""
+    """Metric's prefix"""
 
-    IS_CONNECTED: Final[Gauge] = Gauge(PREFIX + "connected", "機器人是否連接到 Discord", ["shard"])
-    """機器人是否連接到 Discord, 值為 1 或 0"""
+    IS_CONNECTED: Final[Gauge] = Gauge(
+        PREFIX + "connected", "Whether the bot is connected to Discord", ["shard"]
+    )
+    """Whether the bot is connected to Discord, 1 for connected, 0 for disconnected"""
 
     LATENCY: Final[Gauge] = Gauge(
-        PREFIX + "latency_seconds", "機器人連接到 Discord 的延遲", ["shard"]
+        PREFIX + "latency_seconds", "Delay between the bot and Discord", ["shard"]
     )
-    """機器人連接到 Discord 的延遲 (單位: 秒)"""
+    """Delay between the bot and Discord (unit: seconds)"""
 
-    GUILDS: Final[Gauge] = Gauge(PREFIX + "guilds_total", "機器人所在的伺服器總數量")
-    """機器人所在的伺服器總數量"""
+    GUILDS: Final[Gauge] = Gauge(PREFIX + "guilds_total", "Number of guilds the bot is in")
+    """Number of guilds the bot is in"""
 
-    USERS: Final[Gauge] = Gauge(PREFIX + "users_total", "機器人已註冊的使用者總數量")
-    """機器人已註冊的使用者總數量"""
+    USER_INSTALLS: Final[Gauge] = Gauge(PREFIX + "user_installs_total", "Number of user installs")
+    """Number of user installs"""
 
-    COMMANDS: Final[Gauge] = Gauge(PREFIX + "commands_total", "機器人能使用的指令的總數量")
-    """機器人能使用的指令的總數量"""
-
-    INTERACTION_EVENTS: Final[Counter] = Counter(
-        PREFIX + "on_interaction_events",
-        "互動指令 (Interaction) 被呼叫的次數",
-        ["shard", "interaction", "command"],
+    SLASH_COMMANDS: Final[Counter] = Counter(
+        PREFIX + "on_slash_command",
+        "Number of times slash commands are called",
+        ["shard", "command"],
     )
-    """互動指令 (Interaction) 被呼叫的次數"""
+    """Number of times slash commands are called"""
 
-    COMMAND_EVENTS: Final[Counter] = Counter(
-        PREFIX + "on_command_events", "文字指令被呼叫的次數", ["shard", "command"]
-    )
-    """文字指令被呼叫的次數"""
+    CPU_USAGE: Final[Gauge] = Gauge(PREFIX + "cpu_usage_percent", "System CPU usage")
+    """System CPU usage (0 ~ 100%)"""
 
-    CPU_USAGE: Final[Gauge] = Gauge(PREFIX + "cpu_usage_percent", "系統的 CPU 使用率")
-    """系統的 CPU 使用率 (0 ~ 100%)"""
-
-    MEMORY_USAGE: Final[Gauge] = Gauge(PREFIX + "memory_usage_percent", "機器人程序的記憶體使用率")
-    """機器人程序的記憶體使用率 (0 ~ 100%)"""
+    MEMORY_USAGE: Final[Gauge] = Gauge(PREFIX + "memory_usage", "Bot memory usage")
+    """Bot memory usage (unit: MB)"""
 
     PROCESS_START_TIME: Final[Gauge] = Gauge(
-        PREFIX + "process_start_time_seconds", "機器人程序啟動時當下的時間"
+        PREFIX + "process_start_time_seconds", "Time when the bot process started"
     )
-    """機器人程序啟動時當下的時間 (UNIX Timestamp)"""
+    """Time when the bot process started (UNIX Timestamp)"""
 
 
 class PrometheusCog(commands.Cog):
-    def __init__(self, bot: commands.Bot) -> None:
+    def __init__(self, bot: HoyoBuddy) -> None:
         self.bot = bot
+
+    def _get_command_name(self, command: app_commands.Command) -> str:
+        if command.parent is not None:
+            if command.parent.parent is not None:
+                return f"{command.parent.parent.name} {command.parent.name} {command.name}"
+            return f"{command.parent.name} {command.name}"
+        return command.name
+
+    async def cog_load(self) -> None:
+        if self.bot.env == "dev":
+            return
         self.set_metrics_loop.start()
-        self.set_metrics_loop_users.start()
+        self.set_metrics_loop_user_installs.start()
 
     async def cog_unload(self) -> None:
+        if self.bot.env == "dev":
+            return
         self.set_metrics_loop.cancel()
-        self.set_metrics_loop_users.cancel()
+        self.set_metrics_loop_user_installs.cancel()
 
     @tasks.loop(seconds=5)
     async def set_metrics_loop(self) -> None:
-        """循環更新延遲、CPU、記憶體使用率"""
-        if isinstance(self.bot, AutoShardedClient):
-            for shard_id, latency in self.bot.latencies:
-                Metrics.LATENCY.labels(shard_id).set(latency)
-        else:
-            Metrics.LATENCY.labels(None).set(self.bot.latency)
+        """Periodically update CPU usage, memory usage, and latency"""
+        for shard_id, latency in self.bot.latencies:
+            Metrics.LATENCY.labels(shard_id).set(latency)
 
         if isinstance(cpu_percent := psutil.cpu_percent(), float):
             Metrics.CPU_USAGE.set(cpu_percent)
-        if isinstance(memory_percent := psutil.Process().memory_percent(), float):
-            Metrics.MEMORY_USAGE.set(memory_percent)
+
+        Metrics.MEMORY_USAGE.set(self.bot.ram_usage)
 
     @tasks.loop(seconds=300)
-    async def set_metrics_loop_users(self) -> None:
-        """循環更新使用者總數量"""
-        num_of_users = await User.all().count()
-        Metrics.USERS.set(num_of_users)
+    async def set_metrics_loop_user_installs(self) -> None:
+        """Periodically update the number of user installs"""
+        app_info = await self.bot.application_info()
+        user_count = app_info.approximate_user_install_count
+        if user_count is not None:
+            Metrics.USER_INSTALLS.set(user_count)
 
     @set_metrics_loop.before_loop
-    @set_metrics_loop_users.before_loop
+    @set_metrics_loop_user_installs.before_loop
     async def before_set_metrics_loop_users(self) -> None:
         await self.bot.wait_until_ready()
 
     def set_guild_gauges(self) -> None:
-        """更新伺服器總數量"""
-        num_of_guilds = len(self.bot.guilds)
-        Metrics.GUILDS.set(num_of_guilds)
+        """Update the number of guilds the bot is in"""
+        Metrics.GUILDS.set(len(self.bot.guilds))
 
     @commands.Cog.listener()
     async def on_ready(self) -> None:
-        """當機器人準備好時, 設定伺服器數量、連線狀態、可使用指令總數"""
         self.set_guild_gauges()
 
         Metrics.IS_CONNECTED.labels(None).set(1)
 
-        num_of_commands = len([*self.bot.walk_commands(), *self.bot.tree.walk_commands()])
-        Metrics.COMMANDS.set(num_of_commands)
-
         process = psutil.Process()
         Metrics.PROCESS_START_TIME.set(process.create_time())
 
-    # -------------------------------------------------------------
-    # 機器人指令呼叫相關監控
     @commands.Cog.listener()
-    async def on_command(self, ctx: commands.Context) -> None:
-        """當文字指令被呼叫時, 設定 Metric"""
-        shard_id = ctx.guild.shard_id if ctx.guild else None
-        command_name = ctx.command.name if ctx.command else None
-        Metrics.COMMAND_EVENTS.labels(shard_id, command_name).inc()
+    async def on_interaction(self, i: Interaction) -> None:
+        shard_id = i.guild.shard_id if i.guild else None
+        command_name = None
 
-    @commands.Cog.listener()
-    async def on_interaction(self, interaction: Interaction) -> None:
-        """當 Interaction 被呼叫時, 設定 Metric"""
-        shard_id = interaction.guild.shard_id if interaction.guild else None
+        if i.type is InteractionType.application_command:
+            if isinstance(i.command, app_commands.Command):
+                parameters = i.namespace.__dict__
+                command_name = self._get_command_name(i.command)
+                logger.info(f"[Command][{i.user.id}] {command_name}", parameters=parameters)
+            elif isinstance(i.command, app_commands.ContextMenu):
+                command_name = i.command.name
 
-        if interaction.type == InteractionType.application_command:
-            command_name = interaction.command.name if interaction.command else None
-        else:  # 從 View (例如 Button, Dropdown...) 被呼叫
-            command_name = None
-
-        Metrics.INTERACTION_EVENTS.labels(shard_id, interaction.type.name, command_name).inc()
+        if command_name is not None:
+            Metrics.SLASH_COMMANDS.labels(shard_id, command_name).inc()
 
     @commands.Cog.listener()
     async def on_connect(self) -> None:
@@ -173,5 +171,5 @@ class PrometheusCog(commands.Cog):
         self.set_guild_gauges()
 
 
-async def setup(client: commands.Bot) -> None:
+async def setup(client: HoyoBuddy) -> None:
     await client.add_cog(PrometheusCog(client))

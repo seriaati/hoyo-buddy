e4cf551161911beeb40cc2c06752918b1cef19d9
seria <seria.ati@gmail.com>
2025-06-08 10:18:10 +0900

    refactor: Refactor gpy error handling

--- hoyo_buddy/bot/error_handler.py
+++ hoyo_buddy/bot/error_handler.py
@@ -4,11 +4,10 @@ from datetime import timedelta
 from typing import Literal
 
 import discord
-import genshin
 from ambr.exceptions import DataNotFoundError as AmbrDataNotFoundError
 from discord.utils import format_dt
 from enka import errors as enka_errors
-from genshin import errors as genshin_errors
+from genshin import errors as ge
 from hakushin.errors import NotFoundError as HakushinNotFoundError
 from yatta.exceptions import DataNotFoundError as YattaDataNotFoundError
 
@@ -27,79 +26,80 @@ from ..utils import get_now
 
 __all__ = ("get_error_embed",)
 
-GENSHIN_ERROR_CONVERTER: dict[
-    tuple[int, ...], dict[Literal["title", "description", "image"], LocaleStr | str]
+type ErrorInfo = dict[Literal["title", "description", "image"], LocaleStr | str]
+
+
+GPY_ERRORS: dict[
+    type[ge.GenshinException] | tuple[type[ge.GenshinException], ...] | int, ErrorInfo
 ] = {
-    (-2017, -2018): {"title": LocaleStr(key="gift_code_redeemed")},
-    (-5003,): {
+    ge.RedemptionClaimed: {"title": LocaleStr(key="gift_code_redeemed")},
+    ge.AlreadyClaimed: {
         "title": LocaleStr(key="already_claimed_title"),
         "description": LocaleStr(key="already_claimed_description"),
     },
-    (-100, 10001, 10103, -1071): {
+    ge.InvalidCookies: {
         "title": LocaleStr(key="invalid_cookies_title"),
         "description": LocaleStr(key="invalid_cookies_description"),
     },
-    (-3205, -3102): {
+    ge.WrongOTP: {
         "title": LocaleStr(key="invalid_verification_code_title"),
         "description": LocaleStr(key="invalid_verification_code_description"),
     },
-    (-3208, -3203, -3004): {
+    (ge.AccountDoesNotExist, ge.AccountLoginFail): {
         "title": LocaleStr(key="invalid_email_password_title"),
         "description": LocaleStr(key="invalid_email_password_description"),
     },
-    (-3206,): {
+    ge.VerificationCodeRateLimited: {
         "title": LocaleStr(key="verification_code_unavailable_title"),
         "description": LocaleStr(key="verification_code_unavailable_description"),
     },
-    (-3101, -1004): {
+    ge.ActionInCooldown: {
         "title": LocaleStr(key="action_in_cooldown_error_title"),
         "description": LocaleStr(
             key="action_in_cooldown_error_message",
             available_time=format_dt(get_now() + timedelta(minutes=1), "T"),
         ),
     },
-    (-3202,): {
+    ge.AccountHasLocked: {
         "title": LocaleStr(key="account_locked_title"),
         "description": LocaleStr(key="account_locked_description"),
     },
-    (10102,): {
+    ge.DataNotPublic: {
         "title": LocaleStr(key="data_not_public.title"),
         "description": LocaleStr(key="data_not_public.description"),
         "image": "https://raw.githubusercontent.com/seriaati/hoyo-buddy/assets/DataNotPublicTutorial.gif",
     },
-    (-2021, -2011): {"title": LocaleStr(key="redeem_code.ar_too_low")},
-    (30001,): {
+    ge.RedeemGameLevelTooLow: {"title": LocaleStr(key="redeem_code.ar_too_low")},
+    ge.NoNeedGeetest: {
         "title": LocaleStr(key="geetest.no_need"),
         "description": LocaleStr(key="geetest.no_need.description"),
     },
-    tuple(genshin.constants.GEETEST_RETCODES): {
+    ge.GeetestError: {
         "title": LocaleStr(key="geetest.required"),
         "description": LocaleStr(
             geetest_type=EnumStr(GeetestType.REALTIME_NOTES), key="geetest.required.description"
         ),
     },
-    (-1, 10307): {"title": LocaleStr(key="game_maintenance_title")},
-    # Below are custom retcodes for Hoyo Buddy, they don't exist in Hoyo's API
-    (999,): {
-        "title": LocaleStr(key="redeeem_code.cookie_token_expired_title"),
-        "description": LocaleStr(key="redeeem_code.cookie_token_expired_description"),
-    },
-    (1000,): {
-        "title": LocaleStr(key="redeeem_code.cookie_token_refresh_failed_title"),
-        "description": LocaleStr(key="redeeem_code.cookie_token_refresh_failed_description"),
-    },
-    (-9999,): {
+    ge.InternalDatabaseError: {"title": LocaleStr(key="game_maintenance_title")},
+    ge.DailyGeetestTriggered: {
         "title": LocaleStr(key="geetest.required"),
         "description": LocaleStr(
             geetest_type=EnumStr(GeetestType.DAILY_CHECKIN), key="geetest.required.description"
         ),
     },
+    # Below are custom retcodes for Hoyo Buddy, they don't exist in Hoyo's API
+    999: {
+        "title": LocaleStr(key="redeeem_code.cookie_token_expired_title"),
+        "description": LocaleStr(key="redeeem_code.cookie_token_expired_description"),
+    },
+    1000: {
+        "title": LocaleStr(key="redeeem_code.cookie_token_refresh_failed_title"),
+        "description": LocaleStr(key="redeeem_code.cookie_token_refresh_failed_description"),
+    },
 }
 
 
-ENKA_ERROR_CONVERTER: dict[
-    type[enka_errors.EnkaAPIError], dict[Literal["title", "description", "image"], LocaleStr]
-] = {
+ENKA_ERRORS: dict[type[enka_errors.EnkaAPIError], ErrorInfo] = {
     enka_errors.PlayerDoesNotExistError: {
         "title": LocaleStr(key="player_not_found_title"),
         "description": LocaleStr(key="player_not_found_description"),
@@ -117,6 +117,22 @@ ENKA_ERROR_CONVERTER: dict[
         "description": LocaleStr(key="api_request_timeout_description"),
     },
 }
+
+
+def _get_gpy_error_info(error: ge.GenshinException) -> ErrorInfo:
+    for exc_type, info in GPY_ERRORS.items():
+        if isinstance(exc_type, int) and error.retcode == exc_type:
+            return info
+
+        if isinstance(exc_type, tuple):
+            for et in exc_type:
+                if isinstance(error, et):
+                    return info
+
+        if isinstance(exc_type, type) and isinstance(error, exc_type):
+            return info
+
+    return {"title": f"[{error.retcode}] HoYo API Error", "description": error.msg}
 
 
 def get_error_embed(
@@ -147,26 +163,20 @@ def get_error_embed(
             ]
             joined_str = "\n".join(game_strs)
             embed.add_description(joined_str)
-    elif isinstance(error, genshin_errors.GenshinException | enka_errors.EnkaAPIError):
+    elif isinstance(error, ge.GenshinException | enka_errors.EnkaAPIError):
         err_info = None
 
-        if isinstance(error, genshin_errors.GenshinException):
-            retcode = -9999 if isinstance(error, genshin.DailyGeetestTriggered) else error.retcode
-            for codes, info in GENSHIN_ERROR_CONVERTER.items():
-                if retcode in codes:
-                    err_info = info
-                    break
-            if err_info is None:
-                err_info = {"title": f"[{error.retcode}] HoYo API Error", "description": error.msg}
+        if isinstance(error, ge.GenshinException):
+            err_info = _get_gpy_error_info(error)
 
-            if isinstance(error, genshin_errors.VisitsTooFrequently):
+            if isinstance(error, ge.VisitsTooFrequently):
                 # Set as not recognized to get traceback in Sentry
                 recognized = False
 
-            if isinstance(error, genshin_errors.AlreadyClaimed):
+            if isinstance(error, ge.AlreadyClaimed):
                 embed_type = "default"
         else:
-            err_info = ENKA_ERROR_CONVERTER.get(type(error))
+            err_info = ENKA_ERRORS.get(type(error))
 
         if err_info is not None:
             title, description, image = (

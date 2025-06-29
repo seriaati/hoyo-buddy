005da0b1f4d821353b07b8f72e5cb5e1b34f065a
seria <seria.ati@gmail.com>
2025-06-29 11:39:26 +0900

    feat: Add draw func

--- hoyo_buddy/draw/funcs/hoyo/genshin/__init__.py
+++ hoyo_buddy/draw/funcs/hoyo/genshin/__init__.py
@@ -5,6 +5,7 @@ from .build_card import draw_genshin_card
 from .build_card2 import GITempTwoBuildCard
 from .characters import draw_character_card
 from .exploration import ExplorationCard
+from .hard_challenge import HardChallengeCard
 from .img_theater import ImgTheaterCard
 from .notes import draw_genshin_notes_card
 from .team_card import GITeamCard

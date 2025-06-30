bf79beeaf9b6345bac90c84e4b174f6c0c851001
seria <seria.ati@gmail.com>
2025-06-30 16:30:07 +0900

    imp: Format damage value with commas in challenge stats display

--- hoyo_buddy/draw/funcs/hoyo/genshin/hard_challenge.py
+++ hoyo_buddy/draw/funcs/hoyo/genshin/hard_challenge.py
@@ -191,7 +191,7 @@ class HardChallengeCard:
         )
         im.paste(icon, icon_pos, icon)
         drawer.write(
-            str(char.value),
+            f"{char.value:,}",
             size=64,
             style="bold",
             position=(icon_pos[0], icon_pos[1] + icon.height + 64),

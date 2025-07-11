8d8801feaf02902f51451ac5d1f758e78b8aba5f
seria <seria.ati@gmail.com>
2025-07-11 15:37:08 +0900

    Fix impl with pathlib

--- hoyo_buddy/utils/misc.py
+++ hoyo_buddy/utils/misc.py
@@ -284,9 +284,7 @@ def get_static_img_path(image_url: str) -> pathlib.Path:
 
     if parsed_url.query:
         query_hash = hashlib.sha256(parsed_url.query.encode()).hexdigest()[:8]
-        name = full_path.name
-        stem, suffix = os.path.splitext(name)
-        new_name = f"{stem}_{query_hash}{suffix}"
+        new_name = f"{full_path.stem}_{query_hash}{full_path.suffix}"
         full_path = full_path.with_name(new_name)
 
     return STATIC_FOLDER / full_path
